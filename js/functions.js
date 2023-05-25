5831d684ea57150a5493a7e601a13832af7bf600
Sandro Silva <81268287+santripas@users.noreply.github.com>
2023-05-25 20:13:35 -0300

    feito slide para parte de depoimentos

--- js/functions.js
+++ js/functions.js
@@ -124,12 +124,65 @@ $(function(){
         $('.mini-img-wraper').eq(0).click();
     }
 
+    /* Clicar e ir para div de contato com base no atributo goto */
+
+    var directory = '/C:/Users/sandr/OneDrive/Documentos/estudos/projeto5-dankicode/'
+
     $('[goto=contato]').click(function(){
-        $('.nav a').css('color','black');
-        $(this).css('color','#EB2D2D');
-        $('html,body').animate({'scrollTop':$('#contato').offset().top});
+        location.href=directory+'index.html?contato';
         return false;
     })
-    
+
+    checkUrl();
+
+    function checkUrl(){
+        var url = location.href.split('/');
+        var curPage = url[url.length-1].split('?');
+
+        if(curPage[1] != undefined && curPage[1] == 'contato'){
+            $('header nav a').css('color','black');
+            $('footer nav a').css('color','white');
+            $('[goto=contato]').css('color','#EB2D2D');
+            $('html,body').animate({'scrollTop':$('#contato').offset().top});
+        }
+    }
+
+    /* menu responsivo*/
+
+    $('.mobile').click(function(){
+        $(this).find('ul').slideToggle();
+    })
+
+    /* Sistema de navegação nos depoimentos da index.html*/
+
+    var amtDepoimento = $('.depoimento-single p').length;
+    var curIndex = 0;
+
+    iniciarDepoimentos();
+    navegarDepoimentos();
+
+    function iniciarDepoimentos(){
+        $('.depoimento-single p').hide();
+        $('.depoimento-single p').eq(0).show();
+    }
+
+    function navegarDepoimentos(){
+        $('[next]').click(function(){
+            curIndex++;
+            if(curIndex >= amtDepoimento)
+            curIndex = 0;
+            $('.depoimento-single p').hide();
+            $('.depoimento-single p').eq(curIndex).show();
+
+        })
+        $('[prev]').click(function(){
+            curIndex--;
+            if(curIndex < 0)
+            curIndex = amtDepoimento-1;
+            $('.depoimento-single p').hide();
+            $('.depoimento-single p').eq(curIndex).show();
+        })
+
+    }
 
 })
